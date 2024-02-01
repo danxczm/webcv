4e0f9145fcd8e732fd6b3ccec6b32cc5ad98dd62
dan <81313913+danxczm@users.noreply.github.com>
2024-02-01 16:25:19 +0200

    feat: header implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
+import Header from '@/components/header';
+
 import './globals.css';
+
 import { Inter } from 'next/font/google';
 
 const inter = Inter({
@@ -20,11 +23,12 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 pt-28 sm:pt-36`}
       >
-        <div className="relative w-full max-w-lg -z-10 ml-auto mr-auto mt-20">
-          <div className="absolute -top-24 right-44 w-96 h-96 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-50  animate-blob animation-delay-2000"></div>
-          <div className="absolute -top-4 left-80 w-96 h-96 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob animation-delay-4000"></div>
-          <div className="absolute -bottom-15 left-20 w-96 h-96 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob"></div>
-        </div>
+        {/* <div className="relative w-full max-w-lg -z-10 ml-auto mr-auto"> */}
+        <div className="absolute -top-24 right-44 w-96 h-96 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-50  animate-blob animation-delay-2000"></div>
+        <div className="absolute -top-4 left-80 w-96 h-96 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob animation-delay-4000"></div>
+        <div className="absolute -bottom-15 left-20 w-96 h-96 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob"></div>
+        {/* </div> */}
+        <Header />
         {children}
       </body>
     </html>
