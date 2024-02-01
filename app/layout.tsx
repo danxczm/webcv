2953675f98cf0cbd145772035fabd79e42934977
dan <81313913+danxczm@users.noreply.github.com>
2024-02-01 18:08:20 +0200

    feat: photo with emoji

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,16 +13,10 @@ export const metadata = {
   description: 'Front-End Developer Borodavko Bohdan.',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.className} bg-gray-50 text-gray-950 pt-28 sm:pt-36`}
-      >
+      <body className={`${inter.className} bg-gray-50 text-gray-950 pt-28 sm:pt-36`}>
         {/* <div className="relative w-full max-w-lg -z-10 ml-auto mr-auto"> */}
         <div className="absolute -top-24 right-44 w-96 h-96 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-50  animate-blob animation-delay-2000"></div>
         <div className="absolute -top-4 left-80 w-96 h-96 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob animation-delay-4000"></div>
--- components/intro.tsx
+++ components/intro.tsx
@@ -1,5 +1,41 @@
+'use client';
+
 import React from 'react';
 
+import Image from 'next/image';
+
+import { motion } from 'framer-motion';
+
 export default function Intro() {
-  return <section>Intro</section>;
+  return (
+    <section>
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ type: 'spring', duration: 1 }}
+          >
+            <Image
+              src="/me.jpeg"
+              alt="Front-End Developer Dan"
+              width="192"
+              height="192"
+              priority={true}
+              className="h-24 w-24 rounded-full border-[0.35rem] border-white object-cover shadow-xl"
+            />
+            <motion.span
+              animate={{
+                rotate: [0, 60, 10, 50, 10],
+              }}
+              transition={{ delay: 0.7, duration: 0.7 }}
+              className="absolute bottom-0 right-0 text-4xl"
+            >
+              👋🏻
+            </motion.span>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
 }
