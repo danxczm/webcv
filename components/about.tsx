22b6f1ec263d1728e5efa2eaa523ba6946ae248f
dan <81313913+danxczm@users.noreply.github.com>
2024-02-05 10:56:13 +0200

    style: change some animation value

--- components/about.tsx
+++ components/about.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import { motion } from 'framer-motion';
 
 import SectionHeading from './section-heading';
@@ -9,7 +10,7 @@ export default function About() {
       className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 0.175 }}
+      transition={{ delay: 0.3, duration: 0.5 }}
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
--- components/section-divider.tsx
+++ components/section-divider.tsx
@@ -7,7 +7,7 @@ export default function SectionDivider() {
     <motion.div
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 0.125 }}
+      transition={{ delay: 0.5, duration: 0.5 }}
       className="bg-gray-200 my-24 h-16 w-1 rounded-full hidden sm:block"
     ></motion.div>
   );
