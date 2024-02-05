53d94db0d32efaa564e88e67338dc71f49001aee
dan <81313913+danxczm@users.noreply.github.com>
2024-02-05 18:07:35 +0200

    feat: skills section 1.0

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Intro from '@/components/intro';
 import SectionDivider from '@/components/section-divider';
 import About from '@/components/about';
 import Project from '@/components/projects';
+import Skills from '@/components/skills';
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <SectionDivider />
       <About />
       <Project />
+      <Skills />
     </main>
   );
 }
