a12a8b8464f1f15c59e1abdc5d6bb4109d946499
dan <81313913+danxczm@users.noreply.github.com>
2024-02-12 14:22:18 +0200

    feat: about section translated

--- components/about.tsx
+++ components/about.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { motion } from 'framer-motion';
+import { useTranslations } from 'next-intl';
 
 import SectionHeading from './section-heading';
 
@@ -7,6 +8,8 @@ import useSectionInView from '@/lib/hooks';
 
 export default function About() {
   const { ref } = useSectionInView('about');
+
+  const t = useTranslations('About');
 
   return (
     <motion.section
@@ -17,22 +20,25 @@ export default function About() {
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.3, duration: 0.5 }}
     >
-      <SectionHeading>About me</SectionHeading>
+      <SectionHeading>{t('about')}</SectionHeading>
       <p className="mb-3">
-        Law grad turned coding maestro, I dove into the world of programming via an online school
-        "GOIT," mastering <span className="font-medium">front-end web development</span>.{' '}
+        {t('info')}
+        <span className="font-medium">{t('info2')}</span>
       </p>
       <p>
-        <span className="italic">Solving coding puzzles</span> gives me an unbeatable high. Armed
-        with <span className="font-medium">React, Next.js</span>, and a touch of{' '}
-        <span className="font-medium">TypeScript</span>, I'm on the lookout for a full-time software
-        developer role. I am always looking to learn new technologies.
+        <span className="italic">{t('info3')}</span>
+        {t('info4')}
+        <span className="font-medium">{t('info5')}</span>
+        {t('info6')}
+        <span className="font-medium">TypeScript</span>
+        {t('info7')}
       </p>
       <p>
-        Outside the coding <span className="italic text-[#03A062]">matrix</span>, catch me making
-        music, watching movies, or playing with my dog.
+        {t('info8')}
+        <span className="italic text-[#03A062]">{t('info9')}</span>
+        {t('info10')}
       </p>
-      <p>Ready to bring my problem-solving prowess to your team!🚀</p>
+      <p>{t('info11')}</p>
     </motion.section>
   );
 }
